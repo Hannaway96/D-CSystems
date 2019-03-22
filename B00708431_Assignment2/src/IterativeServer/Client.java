771620bae52d406722ff0637b7270e574f67b5d9
Jack <jackhans96@gmail.com>
2019-03-22 14:37:23 +0000

    added validation

--- B00708431_Assignment2/src/IterativeServer/Client.java
+++ B00708431_Assignment2/src/IterativeServer/Client.java
@@ -2,15 +2,17 @@ package IterativeServer;
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class Client {
              
     public static void main(String[] args) {
                
-        String hostName = "Jack-PC";    //Default Host
+        String hostName = "localhost";    //Default Host
         int portNum = 7000;             //Default Port       
         int guessCounter = 0;
         boolean found = false;
+        Scanner userInput = new Scanner(System.in);
         
         // assign host machine name and port to connect to it
         if (args.length != 0) {
@@ -30,26 +32,48 @@ public class Client {
                 BufferedReader is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
             {
                 String fromServer;
-                String userGuess, lowLimit, highLimit;
+                int userGuess = 0, lowLimit = 0, highLimit = 0;
                               
                 try{
                           
-                    BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));                  
+                    //BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));                  
                     System.out.print("Welcome to the number guessing game!\n"
                            + "You will input two numbers, a lower limit and a higher limit.\n"
                            + "I'll think of a number between those two limits and you have to guess it.\n"
                            + "GO!");
                                                          
-                    System.out.println("\nPlease Enter the lower boundary: ");                                               
-                    lowLimit = userInput.readLine();
+                    
+                    
+                    System.out.println("\nPlease Enter the lower boundary: "); 
+                    
+                    //Check user input is an integer only
+                    while(!userInput.hasNextInt()){
+                        System.out.println("Not a valid number");
+                        userInput.next();
+                    }                        
+                    lowLimit = userInput.nextInt();
                     os.println(lowLimit);
-                    os.flush();
-                                       
-                    System.out.println("\nPlease Enter the higher boundary: ");
-                    highLimit = userInput.readLine(); 
+                    os.flush();                                                                                               
+                    
+                    System.out.println("\nPlease Enter the higher boundary: ");                                
+                    boolean validBoundaries = false;
+                    while(validBoundaries  == false){
+                        while(!userInput.hasNextInt()){                       
+                            System.out.println("Not a valid number"); 
+                            userInput.next();
+                        }                                           
+                        if(lowLimit >= userInput.nextInt()){
+                            System.out.println("Lower limit cannot be greater than or equal to higher limit");
+                        }
+                        else{
+                            validBoundaries = true;
+                        }
+                    }
+                    
+                    highLimit = userInput.nextInt(); 
                     os.println(highLimit);
                     os.flush();
-                                       
+                    
                     System.out.println("\nGreat! I'll now think of a number...");                                                           
                     //This is where the user starts guessing
                     while((fromServer = is.readLine()) != null){
@@ -66,7 +90,7 @@ public class Client {
                             break;
                         }   
                                                
-                        userGuess = userInput.readLine();
+                        userGuess = userInput.nextInt();
                         os.println(userGuess);
                         os.flush();      
                         guessCounter++; //increment guess after every message sent                      
